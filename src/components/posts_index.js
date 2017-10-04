5b9b4ddc787f3584ce7eaf1aaa36fcdad44ec1d4
Aniruddh Gopale <anigopale@gmail.com>
2017-10-04 12:36:50 +0530

    action fetchPosts connected to posts index component as props

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }//as soon as component renders, this is called
+  // better to use it with API requests
+
   render () {
     return (
       <div>
@@ -10,4 +17,8 @@ class PostsIndex extends Component {
   }
 }
 
-export default PostsIndex;
+
+
+export default connect(null, { fetchPosts })(PostsIndex);
+//created a props.fetchPosts = fetchPosts action
+// connect function
