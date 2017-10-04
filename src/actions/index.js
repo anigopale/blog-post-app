fe96d238a352cd1e73ea095ed7d8de17151488bf
Aniruddh Gopale <anigopale@gmail.com>
2017-10-04 11:56:00 +0530

    action creator created for fetching posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,15 @@
+import axios from 'axios';
+
 export const FETCH_POSTS =  'fetch_posts';
 
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=heroclip1234';
 
 export function fetchPosts() {
-  return {
-    type: FETCH_POSTS
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
+  return {
+    type: FETCH_POSTS,
+    payload: request
   };
 }
