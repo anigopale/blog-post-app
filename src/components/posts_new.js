7ad9da535f00863ae5bf26bc2aef095355852087
Aniruddh Gopale <anigopale@gmail.com>
2017-10-04 19:22:15 +0530

    redux form wired up with PostsNew component

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render() {
     return (
-      <div>
-        PostsNew!
-      </div>
+      <form>
+        <Field
+          name="title"
+        />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
+// just like connect function, this connects form state with component
