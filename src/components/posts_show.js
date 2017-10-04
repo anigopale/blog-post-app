eecd7cad840ef50f6ac58f0f20b91deacef10327
Aniruddh Gopale <anigopale@gmail.com>
2017-10-05 01:29:13 +0530

    posts_show connected to state and fetchPost

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -3,13 +3,34 @@ import { connect } from 'react-redux';
 import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
   render() {
+    const { post } = this.props;
+
+    if (!post) {
+      return (
+        <div>
+          <h1>Loading...</h1>
+        </div>
+      );
+    }
+
     return (
       <div>
-        Posts Show.
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   }
 }
 
-export default connect(null, { fetchPost })(PostsShow);
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
