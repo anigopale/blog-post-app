d170160f634f6ff3503a06836895f28cd997d3ff
Aniruddh Gopale <anigopale@gmail.com>
2017-10-04 23:37:59 +0530

    action creator for POST API requenst created, hooked it up with PostsNew component

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
   renderField(field) {
@@ -25,7 +27,7 @@ class PostsNew extends Component {
   }// field has all event handlers
 
   onSubmit(values) {
-    console.log(values);
+    this.props.createPost(values);
   }
 
   render() {
@@ -75,5 +77,7 @@ function validate(values) {
 export default reduxForm({
     validate,
     form: 'PostsNewForm'
-})(PostsNew);
+})(
+  connect(null,{ createPost })(PostsNew)
+);
 // just like connect function, this connects form state with component
